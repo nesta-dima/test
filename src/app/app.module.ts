f91a3ab60951dcafbbe854c2a4b9950cb2743aa8
nesta_dima <nesta_dima@mail.ru>
2023-08-30 16:51:26 +0400

    template for company site

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,36 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
 import { MainHeaderComponent } from './components/main-header/main-header.component';
-import { AboutCompanyComponent } from './components/about-company/about-company.component';
 import { AboutCompanyService } from './services/about-company.service';
 import { HttpClientModule } from '@angular/common/http';
-import { CalculatePriceComponent } from './components/calculate-price/calculate-price.component';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
-import { QuestionsComponent } from './components/questions/questions.component';
-import { SocialMediaComponent } from './components/social-media/social-media.component';
-import { FooterComponent } from './components/footer/footer.component';
+
 @NgModule({
 declarations: [
 		AppComponent,
-		HeaderComponent,
 		MainHeaderComponent,
-		AboutCompanyComponent,
-		CalculatePriceComponent,
-    QuestionsComponent,
-    SocialMediaComponent,
-		FooterComponent,
 	],
 	imports: [
 		BrowserModule,
--- src/app/components/questions/question.models.ts
+++
@@ -1,4 +0,0 @@
-export interface Question {
-	title: string;
-	description: string;
-}
