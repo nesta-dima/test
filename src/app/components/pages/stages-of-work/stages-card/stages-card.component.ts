6b672810df255580d1466c151c9870929d1283a0
nesta_dima <nesta_dima@mail.ru>
2023-10-08 14:48:57 +0400

    fixed stages-of-work on mobile

--- src/app/components/pages/stages-of-work/stages-card/stages-card.component.ts
+++ src/app/components/pages/stages-of-work/stages-card/stages-card.component.ts
@@ -1,4 +1,11 @@
-import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  Input,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { IStages } from '../../../../models/index.model';
 
 @Component({
@@ -6,15 +13,31 @@ import { IStages } from '../../../../models/index.model';
   templateUrl: './stages-card.component.html',
   styleUrls: ['./stages-card.component.scss'],
 })
-export class StagesCardComponent {
+export class StagesCardComponent implements OnInit {
   @Input() data: IStages = { title: '', image: '', description: '' };
   @ViewChild('card', { static: true }) card!: ElementRef;
 
+  public getScreenWidth: any;
+
+  @HostListener('window:resize', ['$event'])
+  onWindowResize() {
+    this.getScreenWidth = window.innerWidth;
+  }
+  ngOnInit() {
+    this.getScreenWidth = window.innerWidth;
+  }
   onMouseEnter() {
+    if (this.getScreenWidth < 1280) {
+      return;
+    }
+
     this.card.nativeElement.classList.add('active');
   }
 
   onMouseLeave() {
+    if (this.getScreenWidth < 1280) {
+      return;
+    }
     this.card.nativeElement.classList.remove('active');
   }
 }
